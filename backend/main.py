d1b43d9d8ceedc6265c0147b26908a3c300d9035
gordnzhou <gordonzhou223@gmail.com>
2024-09-14 11:26:50 -0400

    integrated cohere api

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 import motor.motor_asyncio
 from dotenv import load_dotenv
 
+import cohere
+
 app = FastAPI()
 
 load_dotenv()
@@ -15,6 +17,19 @@ load_dotenv()
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.get_database("journal")
 notes_collection = db.get_collection("notes")
+
+cohere_client = cohere.Client(os.environ["COHERE_API_KEY"])
+
+CHAT_HISTORY = {
+    "role": "USER",
+    "message": (
+        "This is a transcription of my speech for a course review. I spoke freely, "
+        "in a stream of consciousness style. I want you to write the transcribed "
+        "text into a coherent and concise paragraph while retaining my voice and "
+        "tone so it still sounds like I wrote it. Only return the summarized text "
+        "and nothing else."
+    )
+}
 
 @app.get("/")
 async def root():
@@ -36,6 +51,11 @@ class Note(BaseModel):
 @app.post("/create_note")
 async def create_note(note: NoteIn):
 
+    response = cohere_client.chat(
+        chat_history=[CHAT_HISTORY],
+        message=note.text,
+    )
+
     note = Note(
         text=note.text, 
         date_posted=datetime.now()
@@ -43,6 +63,6 @@ async def create_note(note: NoteIn):
 
     new_note = await notes_collection.insert_one(note.dict())
 
-    # db_note = await notes_collection.find_one({"_id": result.inserted_id})
+    db_note = await notes_collection.find_one({"_id": new_note.inserted_id})
 
-    return { "status_code": 200 }
+    return db_note
